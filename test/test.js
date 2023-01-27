01026ef79d056ddb3c2bb025bc8b948194ce776c
mohsinpluton <mohsin.pluton@gmail.com>
2023-01-27 19:17:17 +0500

    write testing script of contract

---
+++ test/test.js
@@ -0,0 +1,61 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+
+
+// [
+//   "0x04a10bfd00977f54cc3450c9b25c9b3a502a089eba0097ba35fc33c4ea5fcb54",
+//   "0x9d997719c0a5b5f6db9b8ac69a988be57cf324cb9fffd51dc2c37544bb520d65"
+// ]
+let nft
+let token
+
+describe("Deployment  ", function () {
+  it("Contracts Deploy here", async function () {
+   [per1,per2, per3 ,per4] = await ethers.getSigners();
+
+    const ERC20Token = await ethers.getContractFactory("MyToken");
+    token = await ERC20Token.deploy();
+    console.log(`This is ERC20Token ${token.address} deployed `);
+
+    const NFTContract = await ethers.getContractFactory("Rentable");
+    nft = await NFTContract.deploy(token.address);
+    console.log(`This is NFTContract ${nft.address} deployed `);   
+  });
+});
+
+
+describe("Minting Funtionality ", function () {
+  it("Owner Reserve nft's dont Need to Token", async function(){
+    await nft.mint(per1.address);
+    const balace = await nft.balanceOf(per1.address);
+    console.log("this is after Minting owner Balance:",balace)
+    console.log("this is  minted nft's:",await nft.mintedNFTs());
+    console.log("this is counter ownerReserve:", await nft.ownerReserve());
+    console.log("this is counter tokenIds:",await nft.tokenIds());
+  });
+
+  //Failllllllllllllllllllllllll
+  // it("This call will be fail because sales is not start",async function(){
+  //   await nft.connect(per2).mint(per2.address);
+  //   const balace = await nft.balanceOf(per2.address);
+  //   console.log("this is after Minting owner Balance:",balace)
+  //   console.log("this is  minted nft's:",await nft.mintedNFTs());
+  //   console.log("this is counter tokenIds:",await nft.tokenIds());
+  // })
+  
+  it("this is presale activate call ",async function(){
+    await nft.activePreSale(5);
+    console.log("successfully activate");
+  })
+
+  it("this call (minting) will be fail becasue this is not whitelistaddr:", async function (){
+    await nft.connect(per2).mint(per2.address);    
+  })
+
+  
+
+});
